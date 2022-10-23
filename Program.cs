a0c1f7a4c43ae7d1054a3a44168c346ae3e4e615
21555848 <39001015+21555848@users.noreply.github.com>
2022-10-23 21:22:16 +0200

    frontend changes - initial coommit

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDefaultIdentity<WebAppUser>(options => options.SignIn.Requir
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<WebAppContext>();
 
+builder.Services.AddWebOptimizer();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -45,6 +47,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseWebOptimizer();
+
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
