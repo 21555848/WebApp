77bac169b58df6a3bd83dbe112cdb79e2ce74deb
Anga Aviwe Mhlonyane <mhlonyaneanga@gmail.com>
2022-10-27 12:49:57 +0200

    Index page update, Payment methods added.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,13 @@ namespace WebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Payemnt()
+        {
+            return View();
+        }
+        public IActionResult OnlinePayemnt()
+        {
+            return View();
+        }
     }
 }
